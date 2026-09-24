f4cea6c4de5e78c422206dc0037c04f26753fa97
the-abidkhan <theabidkkhan.me@gmail.com>
2026-09-25 02:13:17 +0600

    component added

---
+++ src/app/my-plan/page.tsx
@@ -0,0 +1,13 @@
+
+import MyPlan from '@/components/MyPlan';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <MyPlan></MyPlan>
+        </div>
+    );
+};
+
+export default page;
